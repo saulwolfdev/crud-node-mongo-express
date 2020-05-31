e0ca30779c9a808ced94c4e531f88cb22a0cfbc2
saul <43302778+saulwolfcode@users.noreply.github.com>
2020-05-31 20:10:54 -0300

    Server

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const bodyParser=require("body-parser");
+const express=require("express");
+const morgan=require("morgan");
+const mongoose=require("mongoose");
+
+const app =express();
+
+const usersRoutes=require("./routes/users");
+
+mongoose.Promise=global.Promise;
+mongoose.connect("mongodb://localhost/rest-api",
+{useMongoClient:true})
+.then(db=>console.log("database is connected"))
+.catch(err=>console.log(err))
+
+//SETTINGS
+app.set("port",process.env.PORT||3001);
+
+//MIDDLEWARES
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+//ROUTES
+app.use("/users",usersRoutes);
+//SERVER
+app.listen(app.get("port"),()=>{
+    console.log("server on port",app.get("port"))
+})
